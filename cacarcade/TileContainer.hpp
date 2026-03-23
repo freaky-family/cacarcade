7ab1b52ac056c99a4413aae6a62eb2f03188c4d7
Nadal <nadal.berthelon@epitech.eu>
2026-03-23 18:56:59 +0100

    [FEAT] Tilecontainer now a struct instead of class

--- cacarcade/TileContainer.hpp
+++ cacarcade/TileContainer.hpp
@@ -6,10 +6,9 @@
 #include <map>
 
 namespace cacarcade {
-    class TileContainer {
-        public:
-            std::map<std::pair<std::size_t, std::size_t>, Tile> _tiles;
-            // First element is the width, second is the height
-            std::pair<std::size_t, std::size_t> _dimension;
+    struct TileContainer {
+        std::map<std::pair<std::size_t, std::size_t>, Tile> tiles;
+        // First element is the width, second is the height
+        std::pair<std::size_t, std::size_t> dimension;
     };
 }
