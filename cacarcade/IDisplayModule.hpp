986096e45f1a8728761e3c6f7c8fbc2f3aaf8564
Alexandre Kubiaczyk <alexandre.kubiaczyk@epitech.eu>
2026-03-31 09:28:27 +0200

    [fix] Remove the Wrong Function 🥀

--- cacarcade/IDisplayModule.hpp
+++ cacarcade/IDisplayModule.hpp
@@ -11,7 +11,7 @@ namespace cacarcade {
         public:
             virtual ~IDisplayModule() = default;
 
-            virtual void close() = 0;
+            virtual void clear() = 0;
 
             virtual std::optional<std::unique_ptr<cacarcade::IEvent>> pollEvent() = 0;
             virtual void displayTiles(cacarcade::TileContainer tiles) = 0;
